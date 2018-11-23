5c20192b9a53cffb24791efde63be5d8765555c1
EDC <838365946@qq.com>
2018-11-23 16:31:45 +0800

    gogo

--- src/main/java/dao/Userdao.java
+++ src/main/java/dao/Userdao.java
@@ -1,8 +1,10 @@
 package dao;
 
+import org.springframework.boot.autoconfigure.jersey.JerseyProperties;
+
 /**
  * Created by Administrator on 2018/11/23.
  */
-public class Userdao {
+public class Userdao extends JerseyProperties.Servlet{
 
 }
